ed6c18ef07da7be1c337bafdc2c4db56ad828a02
Termux and Linux <linuxundtermux@gmail.com>
2024-03-12 21:27:59 +0100

    create

---
+++ termux-custom/main.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+mkdir $HOME/.termux/custom/
+
+chmod +x $HOME/.termux/custom/
+
+# Speichere den Namen der Datei, die als Argument übergeben wurde
+filename="$1"
+
+# Vollständiger Pfad zur Datei im .termux/custom/ Verzeichnis
+fullpath="${HOME}/.termux/custom/${filename}"
+
+# Überprüfe, ob die Datei existiert und im richtigen Verzeichnis liegt
+if [[ -f "$fullpath" ]]; then
+    # Führe die Datei aus, wenn sie existiert
+    bash "$fullpath"
+else
+    # Gebe eine Fehlermeldung aus, wenn die Datei nicht existiert
+    echo "Fehler: Datei '${filename}' nicht im Verzeichnis .termux/custom/ gefunden."
+fi
